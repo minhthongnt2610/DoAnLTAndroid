031ec55713a5f5a687c3142c3ff04b32c66a3b21
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-11-23 14:01:26 +0700

    hoàn thành dial và call

--- app/src/main/java/vn/edu/stu/quanlydouong/Info_Activity.java
+++ app/src/main/java/vn/edu/stu/quanlydouong/Info_Activity.java
@@ -1,12 +1,20 @@
 package vn.edu.stu.quanlydouong;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,17 +27,70 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Info_Activity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button btnDial, btnCall;
+    String phone = "0123456789";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.infoActivity), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
         setSupportActionBar(findViewById(R.id.toolbar));
+        addControls();
+        addEvents();
     }
+
+    private void addEvents() {
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyDial();
+            }
+        });
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyCall();
+            }
+        });
+    }
+
+    private void xulyDial() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(android.net.Uri.parse("tel:" + phone));
+        startActivity(intent);
+    }
+
+    private void xulyCall() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{android.Manifest.permission.CALL_PHONE},
+                    1
+            );
+            return;
+        }
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(android.net.Uri.parse("tel:" + phone));
+        startActivity(intent);
+    }
+
+    private void addControls() {
+        btnDial = findViewById(R.id.btnDial);
+        btnCall = findViewById(R.id.btnCall);
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.top_menu, menu);
